bc140f76a8b4d69c4193e141761150f491ce18f6
doongjohn <doongjohn@gmail.com>
2021-08-27 22:45:52 +0900

    update

--- script.js
+++ script.js
@@ -2,6 +2,23 @@ const elInfo = document.getElementById('info');
 const elChatView = document.getElementById('chat-view');
 const elChatInput = document.getElementById('chat-input');
 
+function chatInputStart() {
+  elChatInput.removeAttribute('readonly');
+  elChatInput.addEventListener('keypress', event => {
+    if (event.ctrlKey && event.key == '\n') {
+      elChatInput.value += '\n';
+      return;
+    }
+
+    if (event.key == 'Enter') {
+      event.preventDefault();
+      sendText(elChatInput.value);
+      createChatBubbleSelf(elChatInput.value);
+      elChatInput.value = '';
+    }
+  });
+}
+
 function createChatBubble(text) {
   const chatBubble = document.createElement('div');
   chatBubble.classList.add('chat-bubble');
@@ -25,108 +42,115 @@ function createChatBubbleInfo(text) {
 }
 
 // p2p data
-let thisPeer = null;
+const peer = new Peer(null, {
+  'iceServers': [
+    // Public STUN server list
+    // https://gist.github.com/mondain/b0ec1cf5f60ae726202e
+    { 'urls': 'stun.services.mozilla.com:3478' },
+    { 'urls': 'stun.stunprotocol.org:3478' },
+    { 'urls': 'stun.l.google.com:19302' },
+    { 'urls': 'stun1.l.google.com:19302' },
+    { 'urls': 'stun2.l.google.com:19302' },
+    { 'urls': 'stun3.l.google.com:19302' },
+    { 'urls': 'stun4.l.google.com:19302' },
+    { 'urls': 'stunserver.org:3478' },
+    { 'urls': 's1.voipstation.jp' },
+    { 'urls': 's2.voipstation.jp' },
+    { 'urls': 's1.taraba.net' },
+    { 'urls': 's2.taraba.net' },
+  ]
+});
 let oppConn = null;
 
-elInfo.innerText = '⚙️ initializing...';
-thisPeer = new Peer();
-thisPeer.on('open', (id) => {
-  // if initializer reloads the page
-  // it will regenerate fresh link
-  const prevID = sessionStorage.getItem('prevID');
-  if (location.hash && prevID) {
-    if (location.hash == prevID) {
-      // you are the initializer
+function main() {
+  elChatInput.setAttribute('readonly', 'true');
+
+  elInfo.innerText = '⚙️ initializing...';
+  peer.on('open', (id) => {
+    const prevID = sessionStorage.getItem('prevID');
+    if (!location.hash || (location.hash && prevID && location.hash == prevID)) {
+      // if you are the initializer
+      // reloading the page will generate a fresh link
       location.hash = btoa(id);
       sessionStorage.setItem('prevID', location.hash);
-      elInfo.innerHTML = `🚀 share your link`;
+      elInfo.innerText = `🚀 share your link`;
       return;
     }
-  }
 
-  if (!location.hash) {
-    // you are the initializer
-    location.hash = btoa(id);
-    sessionStorage.setItem('prevID', location.hash);
-    elInfo.innerHTML = `🚀 share your link`;
-  } else {
     // connect to initializer
     elInfo.innerText = `🔎 connecting...`;
-    connectTo(atob(location.hash.substring(1)));
+    oppConn = connectTo(atob(location.hash.substring(1)));
     oppConn.on('open', () => {
       oppConn.send({
-        sender: thisPeer.id,
+        sender: peer.id,
         type: 'init',
         message: ''
       });
+      chatInputStart();
     });
-  }
-});
-thisPeer.on('connection', (opp) => {
-  console.log('data received!');
-  opp.on('data', (data) => {
-    if (data.type == 'init') {
-      connectTo(data.sender);
-    } else {
-      createChatBubble(data.message);
-    }
   });
-});
+
+  peer.on('connection', (conn) => {
+    conn.on('data', (data) => {
+      console.log('data received!');
+      switch (data.type) {
+        case 'init':
+          // when you are the initializer
+          oppConn = connectTo(data.sender);
+          chatInputStart();
+          break;
+        default:
+          // create chat bubble
+          createChatBubble(data.message);
+          break;
+      }
+    });
+  });
+
+  // on page exit
+  window.addEventListener("beforeunload", event => {
+    alert('Do you really want to exit?');
+    peer?.destroy();
+    window.close();
+  });
+  document.addEventListener("visibilitychange", () => {
+    if (document.visibilityState === 'hidden')
+      peer?.destroy();
+  });
+  window.addEventListener("pagehide", event => {
+    if (!event.persisted)
+      peer?.destroy();
+  });
+}
 
 function connectTo(id) {
   console.log('connecting...');
-  oppConn = thisPeer.connect(id);
-  oppConn.on('open', () => {
+  const conn = peer.connect(id);
+  conn.on('open', () => {
     elInfo.innerHTML = `✔️ connected`;
     createChatBubbleInfo('connection started!');
   });
-  oppConn.on('close', err => {
+  conn.on('close', err => {
     elInfo.innerHTML = `⛔ connection ended`;
     createChatBubbleInfo('connection ended!');
-    oppConn = null;
+    conn = null;
     console.log(err);
   });
-  oppConn.on('error', err => {
-    elInfo.innerHTML = `⛔ connection error`;
-    oppConn = null;
+  conn.on('error', err => {
+    elInfo.innerHTML = `⛔ connection lost`;
+    createChatBubbleInfo('connection lost!');
+    conn = null;
     console.log(err);
   });
+  return conn;
 }
 
 function sendText(text) {
   oppConn.send({
-    sender: thisPeer.id,
+    sender: peer.id,
     type: 'txt',
     message: text
   });
 }
 
-// input event
-elChatInput.addEventListener('keypress', event => {
-  if (!thisPeer || !oppConn) {
-    event.preventDefault();
-    return;
-  }
-  if (event.ctrlKey && event.key == '\n') {
-    elChatInput.value += '\n';
-    return;
-  }
-  if (event.key == 'Enter') {
-    event.preventDefault();
-    sendText(elChatInput.value);
-    createChatBubbleSelf(elChatInput.value);
-    elChatInput.value = '';
-  }
-});
-
-// on page exit
-document.addEventListener("visibilitychange", () => {
-  if (document.visibilityState === 'hidden') {
-    thisPeer.destroy();
-  }
-});
-window.addEventListener("pagehide", event => {
-  if (!event.persisted) {
-    thisPeer?.destroy();
-  }
-}, false);
+main();
